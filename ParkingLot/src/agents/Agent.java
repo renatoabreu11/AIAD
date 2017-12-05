5c5b4f4811a909889c74349122b8a68f84c99ead
William <wnfukunaga@gmail.com>
2017-12-05 16:00:42 +0000

    Added movement to drivers

--- ParkingLot/src/agents/Agent.java
+++ ParkingLot/src/agents/Agent.java
@@ -1,6 +1,12 @@
 package agents;
 
+import java.util.List;
+
 public class Agent {
 	
 	public void step() {};
+	
+	public List<String> getTransportAvailable() {
+		return null;
+	}
 }
--- ParkingLot/src/agents/ParkingLot.java
+++ ParkingLot/src/agents/ParkingLot.java
@@ -1,5 +1,24 @@
 package agents;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot extends Agent {
 	
+	
+	/** A list of agents who live here */
+	private List<Agent> agents;
+
+	public ParkingLot() {
+		this.agents = new ArrayList<Agent>();
+	}
+	
+
+	public void addAgent(Agent a) {
+		this.agents.add(a);
+	}
+
+	public List<Agent> getAgents() {
+		return this.agents;
+	}
 }
